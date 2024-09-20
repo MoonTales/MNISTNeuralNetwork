2cd3d68dcd0479161fcce7931025063b80be1b76
NicroFang <moontalesstudioofficial@gmail.com>
2024-09-19 22:21:18 -0600

    updates

--- A1_submission.py
+++ A1_submission.py
@@ -1,8 +1,9 @@
 # Imports
+import torch
 import torchvision
 from torch.utils.data import DataLoader
 import torch.nn as nn
-
+import torch.nn.functional as F
 
 def logistic_regression(device):
     # Create our classifier model
@@ -16,7 +17,7 @@ def logistic_regression(device):
                 n_epochs = 21 # How many times we pass over the complete training data during training
                 log_interval = 75 # How often we print an update during training
             """
-            def __init__(self, learning_rate=2.3e-2, lambda_value=0.001, n_epochs=21, log_interval=75):
+            def __init__(self, learning_rate=2.3e-2, lambda_value=0.001, n_epochs=5, log_interval=75):
                 self.learning_rate = learning_rate
                 self.lambda_value = lambda_value
                 self.n_epochs = n_epochs
@@ -70,7 +71,7 @@ def logistic_regression(device):
             # Return the DataLoaders
             return train_loader, val_loader, test_loader
 
-        def train_model(self, epoch, data_loader, model, optimizer, lambda_value):
+        def train_model(self, epoch, data_loader, model, optimizer):
             model.train()  # Set model to training mode
             for batch_idx, (data, target) in enumerate(data_loader):
                 data = data.to(device)
@@ -79,7 +80,7 @@ def logistic_regression(device):
                 output = model(data)
                 loss = F.cross_entropy(output, target)
                 # L2 regularization
-                loss += lambda_value * torch.norm(model.fc.weight, 2)
+                loss += self.Hyperparameters.lambda_value * torch.norm(model.fc.weight, 2)
         
                 loss.backward()
                 optimizer.step()
@@ -88,10 +89,11 @@ def logistic_regression(device):
                         epoch, batch_idx * len(data), len(data_loader.dataset),
                                100. * batch_idx / len(data_loader), loss.item()))
 
-        def evaluate(self, data_loader, model, dataset):
+        def evaluate(self, data_loader, model, dataset, print_to_log=True, return_accuracy=False):
             model.eval()  # Set model to evaluation mode
             loss = 0
             correct = 0
+        
             with torch.no_grad():  # Disable gradient calculation
                 for data, target in data_loader:
                     data = data.to(device)
@@ -100,58 +102,26 @@ def logistic_regression(device):
                     pred = output.data.max(1, keepdim=True)[1]
                     correct += pred.eq(target.data.view_as(pred)).sum()
                     loss += F.cross_entropy(output, target, reduction='sum').item()
+        
             loss /= len(data_loader.dataset)
-            print(dataset + ' set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-                loss, correct, len(data_loader.dataset), 100. * correct / len(data_loader.dataset)))
+            accuracy = 100. * correct / len(data_loader.dataset)
+        
+            if print_to_log:
+                print(f'{dataset} set: Avg. loss: {loss:.4f}, Accuracy: {correct}/{len(data_loader.dataset)} ({accuracy:.0f}%)\n')
+        
+            if return_accuracy:
+                return accuracy
 
     LRM = LogisticRegressionModel().to(device)
 
     LRM.evaluate(LRM.validation_loader, LRM, "Validation")
     for epoch in range(1, LRM.Hyperparameters.n_epochs + 1):
-        LRM.train_model(epoch, LRM.train_loader, LRM, LRM.Optimizer, lambda_value=LRM.Hyperparameters.lambda_value)
+        LRM.train_model(epoch, LRM.train_loader, LRM, LRM.Optimizer)
         LRM.evaluate(LRM.validation_loader, LRM, "Validation")
 
     results = {}
     results['model'] = LRM
     return results
-
-
-def create_train_val_loaders_FNN(batch_size):
-    '''
-    Function Taken from the FNN_main.py
-    '''
-    import torch
-    from torch.utils.data import random_split
-    import torchvision
-    """
-
-    :param Params.BatchSize batch_size:
-    :return:
-    """
-
-    CIFAR_training = torchvision.datasets.CIFAR10('.', train=True, download=True,
-                                                  transform=torchvision.transforms.Compose([
-                                                      torchvision.transforms.ToTensor(),
-                                                      torchvision.transforms.Normalize((0.5, 0.5, 0.5),
-                                                                                       (0.5, 0.5, 0.5))]))
-
-    CIFAR_test_set = torchvision.datasets.CIFAR10('.', train=False, download=True,
-                                                  transform=torchvision.transforms.Compose([
-                                                      torchvision.transforms.ToTensor(),
-                                                      torchvision.transforms.Normalize((0.5, 0.5, 0.5),
-                                                                                       (0.5, 0.5, 0.5))]))
-
-    # create a training and a validation set
-    CIFAR_train_set, CIFAR_val_set = random_split(CIFAR_training, [40000, 10000])
-
-    train_loader = torch.utils.data.DataLoader(CIFAR_train_set, batch_size=batch_size.train, shuffle=True)
-
-    val_loader = torch.utils.data.DataLoader(CIFAR_val_set, batch_size=batch_size.val, shuffle=False)
-
-    test_loader = torch.utils.data.DataLoader(CIFAR_test_set,
-                                              batch_size=batch_size.test, shuffle=False)
-
-    return train_loader, val_loader, test_loader
 
 class FNN(nn.Module):
     def __init__(self, loss_type, num_classes):
@@ -188,19 +158,13 @@ class FNN(nn.Module):
 
         return loss
 
-
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-
-
 def tune_hyper_parameter(target_metric, device):
     # Redefine the Models that will be used for testing the hyperparameters
     best_params, best_metric = None, None
 
     # Logistic Regression Model redefined
     class LogisticRegressionModel(nn.Module):
-    
+
         class Hyperparameters:
             """
             Hyperparameters:
@@ -209,28 +173,28 @@ def tune_hyper_parameter(target_metric, device):
                 n_epochs = 21 # How many times we pass over the complete training data during training
                 log_interval = 75 # How often we print an update during training
             """
-            def __init__(self, learning_rate=2.3e-2, lambda_value=0.001, n_epochs=21, log_interval=75):
+            def __init__(self, learning_rate=2.3e-2, lambda_value=0.001, n_epochs=5, log_interval=75):
                 self.learning_rate = learning_rate
                 self.lambda_value = lambda_value
                 self.n_epochs = n_epochs
                 self.log_interval = log_interval
-    
+
         def __init__(self):
             super(LogisticRegressionModel, self).__init__()
             self.fc = nn.Linear(28 * 28, 10)
-    
+
             # -- Create our Other variables -- #
             self.Hyperparameters = self.Hyperparameters()
             transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor(), torchvision.transforms.Normalize((0.1307,), (0.3081,))])
             self.train_loader, self.validation_loader, self.test_loader = self.create_Dataloaders(batch_size=64, val_size=12000, transform=transform)
             self.Optimizer = torch.optim.SGD(self.parameters(), lr=self.Hyperparameters.learning_rate, momentum=0.9)
             # -------------------------------- #
-    
+
         def forward(self, x):
             x = x.view(x.size(0), -1)
             x = self.fc(x)
             return F.softmax(x, dim=1)
-    
+
         def create_Dataloaders(self, batch_size=64, val_size=12000, transform=None):
             # Define transformations [Which was taken from the provided notebook]
             # Load MNIST dataset
@@ -238,32 +202,32 @@ def tune_hyper_parameter(target_metric, device):
                                                         transform=transform)
             test_set = torchvision.datasets.MNIST('/MNIST_dataset/', train=False, download=True, transform=transform)
             # --
-    
+
             # Determine the size of the training set
             num_train_samples = len(full_train_set)
             # --
-    
+
             # Calculate indices for splitting
             train_indices = list(range(num_train_samples))
             val_indices = train_indices[-val_size:]  # Last `val_size` samples for validation
             train_indices = train_indices[:-val_size]  # Rest of the samples for training
             # --
-    
+
             # Create subsets for training and validation using the indices we created above
             train_subset = torch.utils.data.Subset(full_train_set, train_indices)
             val_subset = torch.utils.data.Subset(full_train_set, val_indices)
             # --
-    
+
             # Create DataLoaders for each of our 3 datasets, train, validation, and test
             train_loader = torch.utils.data.DataLoader(train_subset, batch_size=batch_size, shuffle=True)
             val_loader = torch.utils.data.DataLoader(val_subset, batch_size=batch_size, shuffle=True)
             test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=True)
             # --
-    
+
             # Return the DataLoaders
             return train_loader, val_loader, test_loader
-    
-        def train_model(self, epoch, data_loader, model, optimizer, lambda_value):
+
+        def train_model(self, epoch, data_loader, model, optimizer):
             model.train()  # Set model to training mode
             for batch_idx, (data, target) in enumerate(data_loader):
                 data = data.to(device)
@@ -272,19 +236,20 @@ def tune_hyper_parameter(target_metric, device):
                 output = model(data)
                 loss = F.cross_entropy(output, target)
                 # L2 regularization
-                loss += lambda_value * torch.norm(model.fc.weight, 2)
-    
+                loss += self.Hyperparameters.lambda_value * torch.norm(model.fc.weight, 2)
+
                 loss.backward()
                 optimizer.step()
                 if batch_idx % self.Hyperparameters.log_interval == 0:
                     print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                         epoch, batch_idx * len(data), len(data_loader.dataset),
                                100. * batch_idx / len(data_loader), loss.item()))
-    
-        def evaluate(self, data_loader, model, dataset):
+
+        def evaluate(self, data_loader, model, dataset, print_to_log=True, return_accuracy=False):
             model.eval()  # Set model to evaluation mode
             loss = 0
             correct = 0
+
             with torch.no_grad():  # Disable gradient calculation
                 for data, target in data_loader:
                     data = data.to(device)
@@ -293,9 +258,15 @@ def tune_hyper_parameter(target_metric, device):
                     pred = output.data.max(1, keepdim=True)[1]
                     correct += pred.eq(target.data.view_as(pred)).sum()
                     loss += F.cross_entropy(output, target, reduction='sum').item()
+
             loss /= len(data_loader.dataset)
-            print(dataset + ' set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-                loss, correct, len(data_loader.dataset), 100. * correct / len(data_loader.dataset)))
+            accuracy = 100. * correct / len(data_loader.dataset)
+
+            if print_to_log:
+                print(f'{dataset} set: Avg. loss: {loss:.4f}, Accuracy: {correct}/{len(data_loader.dataset)} ({accuracy:.0f}%)\n')
+
+            if return_accuracy:
+                return accuracy
 
     # FNN Model
     class FNN(nn.Module):
@@ -332,7 +303,6 @@ def tune_hyper_parameter(target_metric, device):
                 raise ValueError(f"Unsupported loss type: {self.loss_type}")
 
             return loss
-
     import FNN_main as fnn
     fnn_train_loader, fnn_val_loader, fnn_test_loader = fnn.get_dataloaders(fnn.Params.BatchSize)
 
@@ -375,10 +345,10 @@ def tune_hyper_parameter(target_metric, device):
         LRM = LogisticRegressionModel().to(device)  # Re-initialize the model
         optimizer = torch.optim.Adam(LRM.parameters(), lr=lr)
 
-        for epoch in range(1, 4):  # Use base_epochs_log = 3
-            train(epoch, LRM.train_loader, LRM, optimizer, log_interval=100, device=device)
+        for epoch in range(1, 2):  # Use base_epochs_log = 3
+            LRM.train_model(epoch, LRM.train_loader, LRM, optimizer)
 
-        validation_accuracy = eval(LRM.validation_loader, LRM, device)
+        validation_accuracy = LRM.evaluate(LRM.validation_loader, LRM, device, False, True)
 
         if validation_accuracy > best_accuracy_logistic:
             best_accuracy_logistic = validation_accuracy
@@ -395,8 +365,8 @@ def tune_hyper_parameter(target_metric, device):
         LRM = LogisticRegressionModel().to(device)  # Re-initialize the model
         optimizer = torch.optim.Adam(LRM.parameters(), lr=best_learning_rate_logistic)
 
-        for epoch in range(1, 4):  # Use base_epochs_log = 3
-            train(epoch, LRM.train_loader, LRM, optimizer, log_interval=100, device=device)
+        for epoch in range(1, 2):  # Use base_epochs_log = 3
+            LRM.train_model(epoch, LRM.train_loader, LRM, optimizer)
 
         # Here you would need to adjust for how lambda is incorporated into your model, e.g., regularization.
         validation_accuracy = eval(LRM.validation_loader, LRM, device)
